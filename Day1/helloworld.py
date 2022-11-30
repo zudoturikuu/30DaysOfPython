f3d18ba2baeeff39994cc57218f80daff80370e1
Jun Heng <zudoturiku@duck.com>
2022-11-30 19:12:44 +0800

    completed day 1 of python

---
+++ Day1/helloworld.py
@@ -0,0 +1,28 @@
+
+import math
+
+print(2+3)
+print(2-1)
+print(5/5)
+print(4//5)
+print(100%4)
+print(5*2)
+print(5**4)
+
+print(type(10))
+print(type("string"))
+print(type('string'))
+print(type(2.144444))
+print(type(10+1j))
+print(type([1,2,3,4,5,5,5]))
+print(type({'name':'Sam'}))
+print(type(('mary','john')))
+print(type({1,2,3,4}))
+#this is a comment
+print(3+4)
+print(type(9.8))
+
+#Find an Euclidian distance between (2, 3) and (10, 8)
+
+print(math.sqrt(((2-10) ** 2)+((3-8) ** 2)))
+
