aaa5fe410d40f9aa43a3a81e47debdee1d2c2c3e
Jun Heng <zudoturiku@duck.com>
2022-12-01 23:49:17 +0800

    finished day 2 of python

---
+++ Day 2/VariablesAndFunctions.py
@@ -0,0 +1,57 @@
+#Day 2 already wow what a great course
+
+
+print(min(10,20,30,5))
+print(sum([10,20,30,40,10,98]))
+
+_if = 10
+
+firstname = 'John Wick the 3rd who didnt lose his dog yay'
+lastname = 'fally'
+
+first_name = 'oops i forgot snakecase, too used to camel case in swift'
+age = 9690
+last_name = 'wongers lesgoo'
+is_married = False
+is_dating = False
+
+firstName, lastName, oldness, death = 'John', 'the third', 'immortal', 'never'
+
+print(firstName)
+
+print('Hello World')
+
+print("First Name:", firstname)
+print('First Name Length:', len(firstname))
+
+name = input('first name pls:')
+howOld = input('how old are u matey:')
+
+print(type(first_name))
+
+acceleration_of_free_fall = 9.81
+print(int(acceleration_of_free_fall))
+
+acceleration_of_free_fall_int = int(acceleration_of_free_fall)
+
+print('acceleration_of_free_fall but in int yeah', acceleration_of_free_fall_int)
+
+num1 = 4
+num2 = 5
+numTotal = num1+num2
+numDiff = num1-num2
+numProduct = num1*num2
+numDivide = num1/num2
+numRemainder = num1%num2
+numExponent = num1**num2
+numFloorDivision = num1//num2
+
+radius = input("radius:")
+areaOfCircle = 3.14*(radius**2)
+circum_of_circle = 3.14*60
+
+
+print(numTotal)
+print(areaOfCircle)
+
+help('keywords')
