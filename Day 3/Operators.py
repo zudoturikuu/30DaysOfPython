92904298f44df57cd96ee2e5fecb8066053bcaaa
Jun Heng <zudoturiku@duck.com>
2022-12-03 00:49:01 +0800

    re commit

---
+++ Day 3/Operators.py
@@ -0,0 +1,67 @@
+print('Addition:', 1+2)
+print('Division without remainder', 100//3)
+
+print('Pi', 3.14)
+
+print('complex number', 1+1j)
+print('complex number multiplication', (1+1j) * (1+3j))
+
+a = 3
+b = 9
+
+floor_division = a//b
+remainder = a % b
+exponential = a ** b
+
+print('a // b =', floor_division)
+
+a += 4
+
+print(a)
+
+#Calculating area of cicrleee
+radius = 9.834
+area_of_circle = 3.14 * (radius ** 2)
+
+print('Area of cricle with radius 9.834 =', area_of_circle)
+
+#Calculating weight of object W=MG, my ohysics knowledge coming in handlky lol
+
+weight = 81.6
+gravitational_field_strength = 9.81
+
+weight = weight * gravitational_field_strength
+print(f'My weight:{weight}N')
+
+#Comparison Operators
+
+print(3==2)
+print(4>3)
+print(4<=1)
+
+x = 9.872
+y = -10.2
+
+print(x>y)
+print(y>x)
+
+print(len('pneumonoultramicroscopicsilicovolcanocoliosis') == len('baby'))
+
+print('True == True: ', True == True)
+print('True == False: ', True == False)
+print('False == False:', False == False)
+
+print(1 is not 2)
+print(1 is 1)
+print('J in Jun:',"J" in 'Jun')
+print('J not in Jun:',"J" not in 'Jun')
+print('5 is 4+1:', 5 is 4+1)
+
+#Lgical operators
+
+print(4>3 and 5>4)
+print(4>3 or 10>12)
+print(4<3 or 10>12)
+print(not 3>2)
+print(not False)
+print(not not not not not True)
